c39a658203940afe017eb2d1d2a99a914d46e939
Scot Sexton <rss3509@gmail.com>
2020-05-22 22:12:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
-  array.collection do |string|
+  array.collect do |string|
     test_array << string.reverse
   end
   test_array.join(" ")
